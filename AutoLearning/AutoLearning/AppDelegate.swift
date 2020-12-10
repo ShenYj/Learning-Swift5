dc27486ab491a0091d3873db18add07574fe2799
ShenYj <shenyanjie123@foxmail.com>
2020-12-10 14:16:35 +0800

    reset

--- AutoLearning/AutoLearning/AppDelegate.swift
+++ AutoLearning/AutoLearning/AppDelegate.swift
@@ -10,17 +10,22 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(windowWillClose(window:)), name: NSWindow.willCloseNotification, object: nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+}
 
-
+extension AppDelegate {
+    
+    @objc private func windowWillClose(window: NSWindow) {
+        print("windowClose: \(window)")
+    }
 }
 
